fce8c9142dbbdf5046efc92936d97335db87450f
Maya Jairam <mj20@princeton.edu>
2025-03-06 23:02:13 -0500

    demoform

--- myproject/demoapp/models.py
+++ myproject/demoapp/models.py
@@ -2,10 +2,14 @@ from django.db import models
 from unicodedata import name
 
 # Create your models here.
+#Menu Category
+#Menu
+class MenuCategory(models.Model):
+    menu_category_name = models.CharField(max_length=200)
 class Menu(models.Model):
-    name = models.CharField(max_length = 100)
-    cuisine = models.CharField(max_length = 100)
-    place = models.IntegerField
+    menu_item = models.CharField(max_length = 200, default="Default Item")
+    price = models.IntegerField(default=0)
+    category_id = models.ForeignKey(MenuCategory, on_delete=models.PROTECT, default=None, related_name="category_name")
 
     def __str__(self):
         return self.name + ":" + self.cuisine
